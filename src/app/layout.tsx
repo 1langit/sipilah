6dbc7fd8baba6a55247798c29679852b1ca1109b
Langit Lintang R <126531063+1langit@users.noreply.github.com>
2025-08-02 20:34:09 +0700

    feat(analytics): setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { HeaderSection } from "@/components/shared/header";
 import FooterSection from "@/components/shared/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <HeaderSection />
         {children}
         <FooterSection />
+        <Analytics />
       </body>
     </html>
   );
